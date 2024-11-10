72216271fee91e65f059b0a6413f16abd79a2a6a
GaurishUniyal <162817396+GaurishUniyal@users.noreply.github.com>
2024-11-10 18:56:39 +0530

    Add files via upload

---
+++ Program01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+public:
+    void displaySum(int a, int b) {
+        cout << "The sum of " << a << " and " << b << " is: " << (a + b) << endl;
+    }
+};
+
+int main() {
+    Student student;
+    int num1, num2;
+    
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    
+    student.displaySum(num1, num2);
+
+    return 0;
+}
